44dd1f6021adc7e9bf13c8771ce952741a272944
harsh-sangwan2003 <hsangwan2002@gmail.com>
2022-04-13 21:55:37 +0530

    added

---
+++ React JS/Pre-requisities/Arrow Functions/arrow.js
@@ -0,0 +1,50 @@
+//Write a program to add two numbers using function expression
+let add = function (a, b) {
+
+    return a + b;
+}
+
+let res = add(10, 20);
+console.log(res);
+
+let regularFn = function () {
+
+    console.log("I am an anonymous function");
+}
+regularFn();
+
+let arrowFn = () => console.log("I am an arrow function");
+arrowFn();
+
+/*
+Advantages of arrow functions -
+1.Short syntax
+2.Pure form of function
+*/
+
+let test = () => {
+
+    console.log(this);
+}
+
+let test2 = function () {
+
+    console.log(this);
+}
+test();
+test2();
+
+const obj = {
+
+    test1: () => console.log(this),
+    test2: function () {
+
+        console.log(this);
+
+        let arrow = () => console.log(this);
+        arrow();
+    }
+
+}
+obj.test1();
+obj.test2();
