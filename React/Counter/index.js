8a3064bff6cabf1b6aec3ebece466a581f8a0edd
harsh-sangwan2003 <hsangwan2002@gmail.com>
2022-04-16 10:21:38 +0530

    added

---
+++ React/Counter/index.js
@@ -0,0 +1,44 @@
+class Counter extends React.Component {
+
+    constructor() {
+
+        super();
+
+        this.state = {
+
+            count: 0
+        }
+    }
+
+    //Increment
+    increment = () => {
+
+        this.setState({
+
+            count: this.state.count + 1
+        })
+    }
+
+    //Decrement
+    decrement = () => {
+
+        this.setState({
+
+            count: this.state.count - 1
+        })
+    }
+
+    render() {
+
+        return (
+
+            <> 
+                <h1>{this.state.count}</h1>
+                <button onClick={this.increment}>+</button>
+                <button onClick={this.decrement}>-</button>
+            </>
+        )
+    }
+}
+
+ReactDOM.render(<Counter />, document.getElementById('root'));
